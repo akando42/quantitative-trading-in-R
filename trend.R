30d7792c56bd124f6f4eb7f75f6660751d3244e3
Troy Do <do@infinitylab.world>
2019-06-19 05:37:44 -0500

    Add SMI, SMA50, SMA200 Indicator

--- trend.R
+++ trend.R
@@ -14,6 +14,24 @@ initStrategy <- function(initialDate, fromDate, toDate, portfolioName, portfolio
   initAcct(account.st, portfolios = portfolio.st, initDate = initialDate, currency="USD", initEq = portfolioSize)
   initOrders(portfolio.st, initDate = initialDate)
   strategy(strategy.st, store=TRUE)
+  
+  ## 200 Days Moving Average
+  add.indicator( strategy = strategy.st, name = "SMA", arguments = list(x=quote(Cl(mktdata)),n=200),label = "SMA200")
+  
+  ## 50 Days Moving Average
+  add.indicator( strategy = strategy.st, name = "SMA", arguments = list(x=quote(Cl(mktdata)),n=50),label = "SMA50")
+
+  ## 3 Days Relative Strength Index
+  add.indicator(strategy = strategy.st, name = "RSI", arguments = list(x=quote(Cl(mktdata)), n=3), label = "RSI_3")
 }
 
-initStrategy("2018-01-01", "2018-02-02", "2019-02-02", "MyPortfolio","MPI", 20000, 200, "ROKU")
+indicatorChart <- function(sticker){
+  sticker_sma <- SMA(x=Cl(sticker), n=200)  
+  sticker_rsi <- RSI(price=Cl(sticker), n=3)
+  plot(Cl(sticker))
+  lines(sticker_sma, col = "blue" )
+  lines(sticker_rsi, col = "red" )
+}
+
+initStrategy("2015-01-01", "2015-02-02", "2019-02-02", "MyPortfolio","MPI", 20000, 200, "NFLX")
+indicatorChart("ROKU")
